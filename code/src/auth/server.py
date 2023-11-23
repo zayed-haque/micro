6f583abcba64db0de68abdb1952b0b4be87dfced
boomchillx <zayedsh2003@gmail.com>
2023-11-23 22:16:29 +0530

    functionality of jwt token

--- code/src/auth/server.py
+++ code/src/auth/server.py
@@ -13,6 +13,27 @@ server.config["MYSQL_PASSWORD"]=os.environ.get("MYSQL_PASSWORD")
 server.config["MYSQL_DB"]=os.environ.get("MYSQL_DB")
 server.config["MYSQL_PORT"]=os.environ.get("MYSQL_PORT")
 
-@sever.route("/login",methods=["POST"])
+@server.route("/login",methods=["POST"])
 def login():
     auth=request.authorization
+    if not auth:
+        return "missing credentials",401
+
+    #ecking db for usrname and pass
+    cur=mysql.connection.cursor()
+    res=cur.execute(
+        "SELECT email,password FROM user WHERE email =%s",(auth.username,)
+    )
+    if res>0:
+        user_row=cur.fetchone()
+        email=user_row[0]
+        password=user_row[1]
+        if auth.username!=email or auth.password !=password:
+            return "invalid credentials",401
+        else:
+            return createJWT(auth.username,os.environ.get("JWT_SECRET"),True)
+    else:
+        return "invalid credentials",401
+
+
+
